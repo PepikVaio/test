a05c8a2717f6b581e6d35aac22982d9c296fae24
Wajsar Josef <36114753+PepikVaio@users.noreply.github.com>
2024-01-27 22:28:09 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 COLOR_USER='\e[36m'
@@ -6,22 +8,10 @@ COLOR_SUCCESS='\e[32m'
 COLOR_ERROR='\e[31m'
 NOCOLOR='\e[0m'
 
-APP_BINARY="/usr/bin/xochitl"
-CACHE_DIR="/home/root/.cache/remarkable/xochitl/qmlcache/"
-PATCH_URL="https://raw.githubusercontent.com/mb1986/rm-hacks/main/patches/"
-ZONEINFO_DIR="/usr/share/zoneinfo/"
-
 WGET="wget"
 
-
-dir_is_empty(){
-    [ -d "$1" ] && [ -z "$(ls -A "$1")" ]
-}
-
-
-
-upgrade_wget () {
-    wget_path=/home/root/.local/share/rm-hacks/wget
+upgrade_wget() {
+    wget_path=/home/root/.local/share/test/wget
     wget_remote=http://toltec-dev.org/thirdparty/bin/wget-v1.21.1-1
     wget_checksum=c258140f059d16d24503c62c1fdf747ca843fe4ba8fcd464a6e6bda8c3bbb6b5
 
@@ -30,17 +20,17 @@ upgrade_wget () {
     fi
 
     if ! [ -f "$wget_path" ]; then
-        echo "Fetching secure wget..."
+        echo -e "Fetching secure wget..."
         # Download and compare to hash
         mkdir -p "$(dirname "$wget_path")"
-        if ! wget -q "$wget_remote" --output-document "$wget_path"; then
-            echo "${COLOR_ERROR}Error: Could not fetch wget, make sure you have a stable Wi-Fi connection${NOCOLOR}"
+        if ! "$WGET" -q "$wget_remote" --output-document "$wget_path"; then
+            echo -e "${COLOR_ERROR}Error: Could not fetch wget, make sure you have a stable Wi-Fi connection${NOCOLOR}"
             exit 1
         fi
     fi
 
     if ! sha256sum -c <(echo "$wget_checksum  $wget_path") > /dev/null 2>&1; then
-        echo "${COLOR_ERROR}Error: Invalid checksum for the local wget binary${NOCOLOR}"
+        echo -e "${COLOR_ERROR}Error: Invalid checksum for the local wget binary${NOCOLOR}"
         exit 1
     fi
 
@@ -48,4 +38,4 @@ upgrade_wget () {
     WGET="$wget_path"
 }
 
-
+upgrade_wget
